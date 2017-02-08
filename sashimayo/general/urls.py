6f2d13ac2efd51d48eb29b3923708965f74cbab9
chestermx <chestermx88@gmail.com>
2017-02-08 14:36:15 +0900

    [add]productページの追加

--- sashimayo/general/urls.py
+++ sashimayo/general/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'general'
 urlpatterns = [
     url(r'^$', views.TopView.as_view(), name="top"),
+    url(r'^product/$', views.ProductView.as_view(), name="product"),
 ]
--- sashimayo/general/views.py
+++ sashimayo/general/views.py
@@ -3,3 +3,7 @@ from django.views import generic
 
 class TopView(generic.TemplateView):
     template_name = "top.html"
+
+
+class ProductView(generic.TemplateView):
+    template_name = "product.html"
