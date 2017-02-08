a1360e989adb3534646b5650acb1b86f2370c2a2
chestermx <chestermx88@gmail.com>
2017-02-08 17:16:05 +0900

    [update]Productモデルのcategoryをtagに命名変更

--- sashimayo/general/models.py
+++ sashimayo/general/models.py
@@ -6,4 +6,4 @@ class Product(models.Model):
     title = models.CharField("タイトル", unique=True, max_length=30)
     description = models.CharField("詳細", max_length=240)
     url = models.CharField("URL", max_length=240)
-    category = TaggableManager()
+    tags = TaggableManager()
